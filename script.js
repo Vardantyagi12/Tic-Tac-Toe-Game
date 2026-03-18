acbbebc2a05a0645df96dfb4688e9ca081c09c47
Vardan Tyagi <vardantyagi2004@gmail.com>
2026-03-18 12:04:03 +0530

    Change in js file

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 let box = document.querySelectorAll('.box');
 let newGameBtn = document.querySelector('#new-btn');
 let restartBtn = document.querySelector('#restart-btn');
-let player1 = 'X';
-let player2 = 'O';
-let currentPlayer = player1;
-let gameActive = true;
-
-function handleBoxClick(event) {
-    let box = event.target;
-}
-
-box.forEach((box) => {
-    box.addEventListener('click', handleBoxClick);
-});
-
-newGameBtn.addEventListener('click', () => {
-    box.forEach((box) => {
-        box.textContent = '';
-    });
-    gameActive = true;
-});
-
-restartBtn.addEventListener('click', () => {
-    box.forEach((box) => {
-        box.textContent = '';
-    });
-
-    gameActive = true;
-});
+let trun0 = true;
 
